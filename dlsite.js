9c643c4ad5914758f316c6400f5734ba030296da
Allen Li <darkfeline@felesatra.moe>
2017-10-30 22:21:24 -0700

    Work

--- dlsite.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  'use strict';
-  var code = window.location.href.match(/RJ([0-9]+)/)[1];
-  addLinkbar(code);
-})();
--- hvdb.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  'use strict';
-  var code = window.location.href.match(/WorkDetails\/([0-9]+)/)[1];
-  addLinkbar(code);
-})();
